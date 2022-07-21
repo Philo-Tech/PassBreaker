45bf3351c6cd657cd77910657258fc8902510f36
Philo-Tech <marcin.kargol@icloud.com>
2022-07-21 10:46:02 +0200

    mala zmiana pod konflikt

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,8 +2,8 @@ package app;
 
 public class Main {
      public static void main(String[] args) {
-
          PassBreaker passBreaker = new PassBreaker();
          passBreaker.passBreaker("imie");
+         System.out.println("hello world");
     }
 }
